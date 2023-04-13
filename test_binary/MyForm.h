5194e8be24a19dce90096de338927f7ac06c6edc
Egor5252 <123667969+Egor5252@users.noreply.github.com>
2023-04-14 00:27:53 +0300

    Добавьте файлы проекта.

---
+++ test_binary/MyForm.h
@@ -0,0 +1,133 @@
+#pragma once
+#include <bitset>
+#include <vector>
+#include <iterator>
+
+namespace testbinary {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::IO;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+	private: System::Windows::Forms::RichTextBox^ richTextBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::OpenFileDialog^ openFileDialog1;
+
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->openFileDialog1 = (gcnew System::Windows::Forms::OpenFileDialog());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(13, 13);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 0;
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(13, 40);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(447, 220);
+			this->richTextBox1->TabIndex = 1;
+			this->richTextBox1->Text = L"";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(385, 10);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// openFileDialog1
+			// 
+			this->openFileDialog1->FileName = L"openFileDialog1";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(714, 270);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Resize += gcnew System::EventHandler(this, &MyForm::MyForm_Resize);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->openFileDialog1->ShowDialog();
+		String^ file_path = this->openFileDialog1->FileName;
+		array<unsigned char>^ buffer = File::ReadAllBytes(file_path);
+		int size_of_buffer = buffer->Length;
+		String^ binary = "";
+		int i = 0;
+		std::bitset<8> *bits = new std::bitset<8>[size_of_buffer];
+
+		for each (unsigned char b in buffer) {
+			bits[i] = b;
+			i++;
+		}
+
+		//this->richTextBox1->Text = Convert::ToString(bits[0]);
+		this->textBox1->Text = size_of_buffer.ToString();
+	}
+	private: System::Void MyForm_Resize(System::Object^ sender, System::EventArgs^ e) {
+		this->richTextBox1->Height = Form::Height - 91;
+	}
+};
+}
